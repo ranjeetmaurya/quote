072f7af606bb36c4908807f2154f39688e4b5f1f
ranjeetmaurya <askranjeetmaurya@gmail.com>
2024-11-03 16:14:22 +0530

    crud for qoute

---
+++ app/controllers/qoutes_controller.rb
@@ -0,0 +1,51 @@
+class QoutesController < ApplicationController
+	before_action :set_qoute, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @qoutes = Qoute.all
+  end
+
+  def show
+  end
+
+  def new
+    @qoute = Qoute.new
+  end
+
+  def create
+    @qoute = Qoute.new(qoute_params)
+
+    if @qoute.save
+      redirect_to qoutes_path, notice: "Qoute was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end	
+
+  def update
+    if @qoute.update(qoute_params)
+      redirect_to qoutes_path, notice: "Qoute was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @qoute.destroy
+    redirect_to qoutes_path, notice: "Qoute was successfully destroyed."
+  end
+
+  private
+
+  def set_qoute
+    @qoute = Qoute.find(params[:id])
+  end
+
+  def qoute_params
+    params.require(:qoute).permit(:name)
+  end
+
+end
